c1a9f43cf2aa413baf044f16464c240b970b8529
Debraj <debrajkarmakar010@gmail.com>
2024-03-08 21:12:57 +0530

    readme: updated readme file with steps to run locally

--- src/api/SpotifyAPIConnect.js
+++ src/api/SpotifyAPIConnect.js
@@ -20,5 +20,5 @@ export const configs = {
     headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
     },
-    body: `grant_type=client_credentials&client_id=${import.meta.env.VITE_CLIENT_ID}&client_secret=${import.meta.env.VITE_CLIENT_SECRET}`
+    body: `grant_type=client_credentials&client_id=386d23eddd0f42298f3b63d49ca4bb7f&client_secret=aec72b3f8089489d8a5b04c680972342`
 }
